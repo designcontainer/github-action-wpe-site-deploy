b10c66d68f77dd77d6865ffa1d1893d2b18551c3
RostiMelk <hello@rosti.no>
2021-06-25 11:46:21 +0200

    test -W flag

--- entrypoint.sh
+++ entrypoint.sh
@@ -45,7 +45,7 @@ chmod 600 "$WPE_SSHG_KEY_PRIVATE_PATH"
 chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
 
 # Deploy via SSH
-rsync -aHAXxv --inplace --numeric-ids --progress -e "ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -T -o Compression=no -o strictHostKeyChecking=no -x" --out-format="%n" --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
+rsync -aHAXx -W --numeric-ids --progress -e "ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -T -o Compression=no -o strictHostKeyChecking=no -x" --out-format="%n" --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
 
 # Clear cache
 ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no $WPE_SSH_USER "cd sites/${WPE_ENV_NAME} && wp page-cache flush"
