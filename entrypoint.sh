1309477f66592facd011480fac1e7e0f0173a538
RostiMelk <hello@rosti.no>
2021-06-25 09:41:27 +0200

    Init

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,7 @@ set -e
 : ${WPE_SSHG_KEY_PRIVATE?Required secret not set.}
 : ${WPE_SSHG_KEY_PUBLIC?Required secret not set.}
 
-#SSH Key Vars 
+#SSH Key Vars
 SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
 WPE_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
@@ -14,9 +14,9 @@ WPE_SSHG_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
 
 #Deploy Vars
 WPE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
-if [ -n "$TPO_PATH" ]; then 
+if [ -n "$TPO_PATH" ]; then
     DIR_PATH="$TPO_PATH"
-else 
+else
     DIR_PATH=""
 fi
 
@@ -38,15 +38,16 @@ ssh-keyscan -t rsa "$WPE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 echo "$WPE_SSHG_KEY_PRIVATE" > "$WPE_SSHG_KEY_PRIVATE_PATH"
 echo "$WPE_SSHG_KEY_PUBLIC" > "$WPE_SSHG_KEY_PUBLIC_PATH"
 
-#Set Key Perms 
+#Set Key Perms
 chmod 700 "$SSH_PATH"
 chmod 644 "$KNOWN_HOSTS_PATH"
 chmod 600 "$WPE_SSHG_KEY_PRIVATE_PATH"
 chmod 644 "$WPE_SSHG_KEY_PUBLIC_PATH"
 
 # Deploy via SSH
-rsync --rsh="ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
+rsync -aHAXxv --numeric-ids --progress -e "ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -T -c arcfour -o Compression=no -o strictHostKeyChecking=no -x" --exclude=".*" $SRC_PATH "$WPE_DESTINATION"
 
-# Clear cache 
+
+# Clear cache
 ssh -v -p 22 -i ${WPE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no $WPE_SSH_USER "cd sites/${WPE_ENV_NAME} && wp page-cache flush"
 echo "SUCCESS: Site has been deployed and cache has been flushed."
